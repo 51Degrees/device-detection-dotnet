4f0c43d228f47d6a317c5bc4405280a4bf61ce32
Steve Ballantine <Stephen@51degrees.com>
2023-03-17 14:14:56 +0000

    FEAT: Add utility to generate complete configuration file with all options included. #5484

---
+++ Tools/GenerateConfig/Helpers.cs
@@ -0,0 +1,119 @@
+﻿using FiftyOne.Pipeline.Core.Configuration;
+using FiftyOne.Pipeline.Core.FlowElements;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace GenerateConfig
+{
+    internal static class Helpers
+    {
+        /// <summary>
+        /// Use reflection to get all element builders.
+        /// These are defined as any type that has a Build method 
+        /// where the return type is or implements IFlowElement.
+        /// These will be used when building a pipeline from configuration.
+        /// </summary>
+        public static List<Type> GetAvailableElementBuilders()
+        {
+            var builders = new List<Type>();
+            // Get all loaded types
+            foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies()
+#if DEBUG
+                // Exclude VisualStudio assemblies
+                .Where(a => a.FullName != null && 
+                    a.FullName.StartsWith("Microsoft.VisualStudio",
+                        StringComparison.OrdinalIgnoreCase) == false)
+#endif
+                )
+            {
+                // Exclude dynamic assemblies
+                if (assembly.IsDynamic == false)
+                {
+                    // Get all types that have..
+                    builders.AddRange(assembly.GetTypes()
+                        .Where(t => t.IsAbstract == false && t.GetMethods()
+                            // ..a method called 'Build'..
+                            .Any(m => m.Name == "Build" &&
+                            // ..where the return type is or implements IFlowElement
+                            (m.ReturnType == typeof(IFlowElement) ||
+                            m.ReturnType.GetInterfaces().Contains(typeof(IFlowElement)))))
+                    .ToList());
+                }
+            }
+
+            return builders;
+        }
+
+        public static void AppendConfigForElementBuilder(Utf8JsonWriter writer, Type builder)
+        {
+            writer.WriteStartObject();
+
+            writer.WriteString(nameof(ElementOptions.BuilderName), builder.Name);
+
+            writer.WritePropertyName(nameof(ElementOptions.BuildParameters));
+            writer.WriteStartObject();
+
+            AppendPropertiesFromSetMethods(writer, builder);
+            AppendPropertiesFromBuildMethods(writer, builder);
+
+            writer.WriteEndObject();
+
+            writer.WriteEndObject();
+        }
+
+        public static void AppendConfigForPipelineBuilder(Utf8JsonWriter writer, Type builder)
+        {
+            writer.WritePropertyName(nameof(PipelineOptions.PipelineBuilderParameters));
+            writer.WriteStartObject();
+
+            AppendPropertiesFromSetMethods(writer, builder);
+            AppendPropertiesFromBuildMethods(writer, builder);
+
+            writer.WriteEndObject();
+        }
+
+        private static void AppendPropertiesFromSetMethods(Utf8JsonWriter writer, Type builder)
+        {
+            // TODO - add defaults and comments
+            var setMethods = builder.GetMethods().Where(m => m.GetParameters().Length == 1 && m.Name.StartsWith("Set"));
+            foreach (var method in setMethods)
+            {
+                var type = method.GetParameters()[0].ParameterType;
+                if (type == typeof(string))
+                {
+                    writer.WriteString(method.Name, "");
+                }
+                else if (type == typeof(bool))
+                {
+                    writer.WriteBoolean(method.Name, false);
+                }
+                else if (type == typeof(int))
+                {
+                    writer.WriteNumber(method.Name, 0);
+                }
+                else
+                {
+                    writer.WriteString(method.Name, $"Unexpected type - {type.Name}");
+                }
+            }
+        }
+        private static void AppendPropertiesFromBuildMethods(Utf8JsonWriter writer, Type builder)
+        {
+            // TODO - add defaults and comments
+            var buildMethods = builder.GetMethods().Where(m => m.Name == "Build");
+            foreach (var method in buildMethods)
+            {
+                foreach (var parameter in method.GetParameters())
+                {
+                    writer.WriteString(parameter.Name ?? "NULL", "");
+                }
+            }
+        }
+    }
+}
---
+++ Tools/GenerateConfig/Program.cs
@@ -0,0 +1,86 @@
+﻿
+using FiftyOne.DeviceDetection;
+using FiftyOne.DeviceDetection.Apple;
+using FiftyOne.DeviceDetection.Cloud.FlowElements;
+using FiftyOne.DeviceDetection.Hash.Engine.OnPremise.FlowElements;
+using FiftyOne.DeviceDetection.Uach;
+using FiftyOne.Pipeline.CloudRequestEngine.FlowElements;
+using FiftyOne.Pipeline.Core.Configuration;
+using FiftyOne.Pipeline.Engines.FiftyOne.FlowElements;
+using FiftyOne.Pipeline.JavaScriptBuilder.FlowElement;
+using FiftyOne.Pipeline.JsonBuilder.FlowElement;
+using FiftyOne.Pipeline.Web.Shared;
+using GenerateConfig;
+using System.Text.Json;
+
+var writerOptions = new JsonWriterOptions
+{
+    Indented = true
+};
+
+// builder we want to appear in a specific order at the start of the file.
+var firstBuilders = new Type[]
+{
+    typeof(SequenceElementBuilder),
+    typeof(ShareUsageBuilder),
+    typeof(AppleProfileEngineBuilder),
+    typeof(UachJsConversionElementBuilder),
+    typeof(CloudRequestEngineBuilder)
+};
+// builder we want to appear in a specific order at the end of the file.
+var lastBuilders = new Type[]
+{
+    typeof(JsonBuilderElementBuilder),
+    typeof(JavaScriptBuilderElementBuilder),
+    typeof(SetHeadersElementBuilder)
+};
+
+// This isn't actually used for anything other than ensuring the relevant types are
+// loaded into the AppDomain so that reflection can find them.
+var middleBuilders = new Type[]
+{
+    typeof(DeviceDetectionCloudEngineBuilder),
+    typeof(HardwareProfileCloudEngineBuilder),
+    typeof(DeviceDetectionHashEngineBuilder),
+};
+
+using (FileStream fs = File.Create("output.json"))
+using (var writer = new Utf8JsonWriter(fs, writerOptions))
+{
+    writer.WriteStartObject();
+    writer.WritePropertyName(nameof(PipelineOptions));
+    writer.WriteStartObject();
+    writer.WritePropertyName(nameof(PipelineOptions.Elements));
+    writer.WriteStartArray();
+
+    // Add an entry for each builder
+    foreach (var builder in firstBuilders)
+    {
+        Helpers.AppendConfigForElementBuilder(writer, builder);
+    }
+    foreach (var builder in Helpers.GetAvailableElementBuilders()
+        .Where(b => firstBuilders.Contains(b) == false && lastBuilders.Contains(b) == false))
+    {
+        Helpers.AppendConfigForElementBuilder(writer, builder);
+    }
+    foreach (var builder in lastBuilders)
+    {
+        Helpers.AppendConfigForElementBuilder(writer, builder);
+    }
+    writer.WriteEndArray();
+
+    // Add pipeline builder options.
+    // FiftyOnePipelineBuilder is the default builder used by the web integration, so we're using that one here.
+    Helpers.AppendConfigForPipelineBuilder(writer, typeof(FiftyOnePipelineBuilder));
+
+    // Add web integration options.
+    var options = new PipelineWebIntegrationOptions();
+    // TODO - add comments
+    writer.WriteBoolean(nameof(PipelineWebIntegrationOptions.ClientSideEvidenceEnabled), options.ClientSideEvidenceEnabled);
+    writer.WriteBoolean(nameof(PipelineWebIntegrationOptions.UseAsyncScript), options.UseAsyncScript);
+    writer.WriteBoolean(nameof(PipelineWebIntegrationOptions.UseSetHeaderProperties), options.UseSetHeaderProperties);
+
+    writer.WriteEndObject();
+    writer.WriteEndObject();
+
+}
