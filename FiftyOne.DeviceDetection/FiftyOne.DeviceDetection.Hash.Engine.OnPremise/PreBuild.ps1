56def4d687390c04a34e6d444e766dbf9e53a40b
Ben51Degrees <ben@51degrees.com>
2023-04-11 10:56:21 +0100

    BUILD: Prebuild is now powershell and calls the CMake. csproj file has been updated with correct paths.

---
+++ FiftyOne.DeviceDetection/FiftyOne.DeviceDetection.Hash.Engine.OnPremise/PreBuild.ps1
@@ -0,0 +1,36 @@
+﻿
+Param (
+	[string]$BuildType = 'Release',
+	[string]$Arch = 'x64'
+)
+
+Push-Location ..
+
+try {
+
+	if ($(Test-Path -Path build) -eq $False) {
+	
+		mkdir build
+
+	}
+
+	Push-Location build
+	
+	try {
+
+		cmake .. -DCMAKE_BUILD_TYPE=$BuildType -A $Arch
+		cmake --build . -t fiftyone-hash-dotnet
+
+	}
+	finally {
+		
+		Pop-Location
+
+	}
+
+}
+finally {
+
+	Pop-Location
+
+}
