f0603b9bc71b241180acb6411752657c03b6879c
51DCI <CI@51Degrees.com>
2023-05-18 10:33:00 +0100

    BUG: fix for the pre build error on ubuntu

--- ci/run-unit-tests.ps1
+++ ci/run-unit-tests.ps1
@@ -4,9 +4,10 @@ param(
     [string]$ProjectDir = ".",
     [string]$Name = "Release_x64",
     [string]$Configuration = "Release",
-    [string]$Arch = "x64"
+    [string]$Arch = "x64",
+    [string]$BuildMethod = "dotnet"
 )
 
-./dotnet/run-unit-tests.ps1 -RepoName $RepoName -ProjectDir $ProjectDir -Name $Name -Configuration $Configuration -Arch $Arch
+./dotnet/run-unit-tests.ps1 -RepoName $RepoName -ProjectDir $ProjectDir -Name $Name -Configuration $Configuration -Arch $Arch -BuildMethod $BuildMethod
 
 exit $LASTEXITCODE
