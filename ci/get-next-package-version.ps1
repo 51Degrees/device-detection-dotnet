95cb5bd7884090696a4c2e416ba7f6458b0209ab
51DCI <CI@51Degrees.com>
2023-05-23 15:35:07 +0100

    FEAT: Add get next package version

---
+++ ci/get-next-package-version.ps1
@@ -0,0 +1,11 @@
+
+param (
+    [Parameter(Mandatory=$true)]
+    [string]$VariableName,
+    [string]$RepoName
+)
+
+./dotnet/get-next-package-version.ps1 -RepoName $RepoName -VariableName $VariableName
+
+
+exit $LASTEXITCODE
