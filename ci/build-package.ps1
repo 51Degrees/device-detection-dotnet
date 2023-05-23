6f13b922f23c2e05a4c591d94ceab278b18bd68d
Artur51D <106963910+Artur51D@users.noreply.github.com>
2023-05-23 16:36:40 +0100

    Update build-package.ps1

--- ci/build-package.ps1
+++ ci/build-package.ps1
@@ -8,6 +8,15 @@ param(
     [string]$Version
 )
 
+# Path to where the dll files are downloaded for all the platfoms
+$PackageFilesPath = [IO.Path]::Combine($pwd, "package-files")
+
+# Copy files over from target to package-files folder
+$Files = Get-ChildItem -Path $PackageFilesPath/* -Recurse -Include "linux", "windows" 
+foreach($file in $Files){
+    Copy-Item -Path $file -Destination $PackageFilesPath -Recurse -Force
+}
+
 ./dotnet/build-package-nuget.ps1 -RepoName $RepoName -ProjectDir $ProjectDir -Name $Name -Configuration $Configuration -Arch $Arch -Version $Version -SolutionName "FiftyOne.DeviceDetection"
 
 exit $LASTEXITCODE
