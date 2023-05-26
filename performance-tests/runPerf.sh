22c079233f382ed980365a032d8a12727b911c69
Artur51D <106963910+Artur51D@users.noreply.github.com>
2023-05-26 16:25:54 +0100

    Update runPerf.sh

--- performance-tests/runPerf.sh
+++ performance-tests/runPerf.sh
@@ -2,7 +2,7 @@
 
 # Constants
 PASSES=20000
-PROJECT="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+PROJECT="$(cd "$(dirname "$(dirname "${BASH_SOURCE[0]}")")" >/dev/null 2>&1 && pwd)"
 HOST=localhost:5000
 CAL=calibrate
 PRO=process
