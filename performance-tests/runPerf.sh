f50f7ccd5fa469212fcfd8a015fb7ce946d8ff71
Artur51D <106963910+Artur51D@users.noreply.github.com>
2023-05-26 15:59:21 +0100

    Update runPerf.sh

--- performance-tests/runPerf.sh
+++ performance-tests/runPerf.sh
@@ -2,14 +2,10 @@
 
 # Constants
 PASSES=20000
-PROJECT=..
+PROJECT="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 HOST=localhost:5000
 CAL=calibrate
 PRO=process
 PERF=./ApacheBench-prefix/src/ApacheBench-build/bin/runPerf.sh
-
-pwd
-
-ls ..
 
 $PERF -n $PASSES -s "dotnet run --project $PROJECT" -c $CAL -p $PRO -h $HOST
