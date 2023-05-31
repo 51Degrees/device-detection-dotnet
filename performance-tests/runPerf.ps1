6b5d4065d7fa1b34b1cbd17990d9306912692c66
Artur51D <106963910+Artur51D@users.noreply.github.com>
2023-05-31 15:47:54 +0100

    Update runPerf.ps1

--- performance-tests/runPerf.ps1
+++ performance-tests/runPerf.ps1
@@ -28,11 +28,9 @@ $scriptRoot = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
 
 # Constants
 $PASSES=20000
-$DLLPATH="$scriptRoot/../"
-$DLL="output/performance_tests.dll"
 $SERVICEHOST="localhost:5000"
 $CAL="calibrate"
 $PRO="process"
 $PERF="$scriptRoot/ApacheBench-prefix/src/ApacheBench-build/bin/runPerf.ps1"
 
-Invoke-Expression "$PERF -n $PASSES -s 'Set-Location $DLLPATH; $d $DLL' -c $CAL -p $PRO -h $SERVICEHOST"
+Invoke-Expression "$PERF -n $PASSES -s '$pwd/../output/performance_tests.exe' -c $CAL -p $PRO -h $SERVICEHOST"
