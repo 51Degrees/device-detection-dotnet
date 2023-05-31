c220612e150d815541c519b2ec7ad44d32fc38b5
Artur51D <106963910+Artur51D@users.noreply.github.com>
2023-05-31 17:35:56 +0100

    Update run-integration-tests.ps1

--- ci/run-integration-tests.ps1
+++ ci/run-integration-tests.ps1
@@ -1,75 +1,75 @@
-# param(
-#     [Parameter(Mandatory=$true)]
-#     [string]$RepoName,
-#     [string]$ProjectDir = ".",
-#     [string]$Name = "Release_x64",
-#     [string]$Configuration = "CoreRelease",
-#     [string]$Arch = "x64",
-#     [string]$Version,
-#     [string]$BuildMethod,
-#     [string]$OrgName
-# )
+param(
+    [Parameter(Mandatory=$true)]
+    [string]$RepoName,
+    [string]$ProjectDir = ".",
+    [string]$Name = "Release_x64",
+    [string]$Configuration = "CoreRelease",
+    [string]$Arch = "x64",
+    [string]$Version,
+    [string]$BuildMethod,
+    [string]$OrgName
+)
 
-# $NugetPackageFolder = [IO.Path]::Combine($pwd, "package")
-# $EvidenceFiles = [IO.Path]::Combine($pwd, $RepoName,"FiftyOne.DeviceDetection", "device-detection-cxx", "device-detection-data")
-# $ExamplesRepoName = "device-detection-dotnet-examples"
-# $ExamplesRepoPath = [IO.Path]::Combine($pwd, $ExamplesRepoName)
+$NugetPackageFolder = [IO.Path]::Combine($pwd, "package")
+$EvidenceFiles = [IO.Path]::Combine($pwd, $RepoName,"FiftyOne.DeviceDetection", "device-detection-cxx", "device-detection-data")
+$ExamplesRepoName = "device-detection-dotnet-examples"
+$ExamplesRepoPath = [IO.Path]::Combine($pwd, $ExamplesRepoName)
 
-# try {
-#     if([String]::IsNullOrEmpty($Version) -eq $False) { 
-#         Write-Output "Cloning '$ExamplesRepoName'"
-#         ./steps/clone-repo.ps1 -RepoName $ExamplesRepoName -OrgName $OrgName
+try {
+    if([String]::IsNullOrEmpty($Version) -eq $False) { 
+        Write-Output "Cloning '$ExamplesRepoName'"
+        ./steps/clone-repo.ps1 -RepoName $ExamplesRepoName -OrgName $OrgName
         
-#         Write-Output "Moving TAC file for examples"
-#         $TacFile = [IO.Path]::Combine($EvidenceFiles, "TAC-HashV41.hash") 
-#         Copy-Item $TacFile device-detection-dotnet-examples/device-detection-data/TAC-HashV41.hash
+        Write-Output "Moving TAC file for examples"
+        $TacFile = [IO.Path]::Combine($EvidenceFiles, "TAC-HashV41.hash") 
+        Copy-Item $TacFile device-detection-dotnet-examples/device-detection-data/TAC-HashV41.hash
 
-#         Write-Output "Moving evidence files for examples"
-#         $UAFile = [IO.Path]::Combine($EvidenceFiles, "20000 User Agents.csv") 
-#         $EvidenceFile = [IO.Path]::Combine($EvidenceFiles, "20000 Evidence Records.yml")
-#         Copy-Item $UAFile "device-detection-dotnet-examples/device-detection-data/20000 User Agents.csv"
-#         Copy-Item $EvidenceFile "device-detection-dotnet-examples/device-detection-data/20000 Evidence Records.yml"
+        Write-Output "Moving evidence files for examples"
+        $UAFile = [IO.Path]::Combine($EvidenceFiles, "20000 User Agents.csv") 
+        $EvidenceFile = [IO.Path]::Combine($EvidenceFiles, "20000 Evidence Records.yml")
+        Copy-Item $UAFile "device-detection-dotnet-examples/device-detection-data/20000 User Agents.csv"
+        Copy-Item $EvidenceFile "device-detection-dotnet-examples/device-detection-data/20000 Evidence Records.yml"
 
-#         $ExamplesProject = [IO.Path]::Combine($ExamplesRepoPath, "Examples", "ExampleBase")
-#         Push-Location $ExamplesRepoPath
+        $ExamplesProject = [IO.Path]::Combine($ExamplesRepoPath, "Examples", "ExampleBase")
+        Push-Location $ExamplesRepoPath
         
-#         nuget restore
+        nuget restore
 
-#         Write-Output "Leaving '$ExamplesRepoPath'"
-#         Pop-Location
+        Write-Output "Leaving '$ExamplesRepoPath'"
+        Pop-Location
         
-#         $LocalFeed = [IO.Path]::Combine($Home, ".nuget", "packages")
-#         ls $LocalFeed
+        $LocalFeed = [IO.Path]::Combine($Home, ".nuget", "packages")
+        ls $LocalFeed
         
-#         Push-Location "package"
+        Push-Location "package"
 
-#         dotnet nuget push "*.nupkg" -s "$LocalFeed"
+        dotnet nuget push "*.nupkg" -s "$LocalFeed"
 
-#         Pop-Location
+        Pop-Location
 
-#         Write-Output "Entering '$ExamplesProject'"
-#         Push-Location $ExamplesProject
+        Write-Output "Entering '$ExamplesProject'"
+        Push-Location $ExamplesProject
         
-#         # Change the dependency version to the locally build Nuget package
-#         Write-Output "Setting the version of the DeviceDetection package to '$Version'"
-#         dotnet add package "FiftyOne.DeviceDetection" --version $Version --source "$LocalFeed" 
+        # Change the dependency version to the locally build Nuget package
+        Write-Output "Setting the version of the DeviceDetection package to '$Version'"
+        dotnet add package "FiftyOne.DeviceDetection" --version $Version --source "$LocalFeed" 
 
-#         Write-Output "Leaving '$ExamplesProject'"
-#         Pop-Location
+        Write-Output "Leaving '$ExamplesProject'"
+        Pop-Location
         
-#         Write-Output "Rebuilding project with following configuration '$Configuration|$Arch|$BuildMethod'"
-#         ./device-detection-dotnet-examples/ci/build-project.ps1 -RepoName $ExamplesRepoName -Name $Name -Configuration $Configuration -Arch $Arch -BuildMethod $BuildMethod
-#         Write-Output "Testing Examples Project"
-#         ./dotnet/run-unit-tests.ps1 -RepoName $ExamplesRepoName -ProjectDir $ProjectDir -Name $Name -Configuration $Configuration -Arch $Arch -BuildMethod $BuildMethod -Filter ".*Tests(|\.Web)\.dll" -OutputFolder "integration"
-#     } 
-#     else{
-#         Write-Output "Not running integration tests at this stage."
-#     }
-# }
+        Write-Output "Rebuilding project with following configuration '$Configuration|$Arch|$BuildMethod'"
+        ./device-detection-dotnet-examples/ci/build-project.ps1 -RepoName $ExamplesRepoName -Name $Name -Configuration $Configuration -Arch $Arch -BuildMethod $BuildMethod
+        Write-Output "Testing Examples Project"
+        ./dotnet/run-unit-tests.ps1 -RepoName $ExamplesRepoName -ProjectDir $ProjectDir -Name $Name -Configuration $Configuration -Arch $Arch -BuildMethod $BuildMethod -Filter ".*Tests(|\.Web)\.dll" -OutputFolder "integration"
+    } 
+    else{
+        Write-Output "Not running integration tests at this stage."
+    }
+}
 
-# finally {
+finally {
 
 
-# }
+}
 
-# exit $LASTEXITCODE
+exit $LASTEXITCODE
