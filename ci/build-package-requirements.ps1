21bf5b2069d1c8ac5505bb62da3cb3e0bd67f99d
Artur51D <106963910+Artur51D@users.noreply.github.com>
2023-06-01 15:27:02 +0100

    Update build-package-requirements.ps1

--- ci/build-package-requirements.ps1
+++ ci/build-package-requirements.ps1
@@ -1,12 +1,20 @@
 param(
     [string]$ProjectDir = "FiftyOne.DeviceDetection",
     [string]$Name = "Release_x64",
+    [Parameter(Mandatory=$true)]
     [string]$RepoName,
     [string]$Arch
 )
 
-./dotnet/build-package-requirement.ps1 -RepoName $RepoName -ProjectDir $ProjectDir -Name $Name -Configuration "Release" -Arch $Arch
-
+if($IsLinux){
+    $Archs = @("x86", "x64")
+    foreach($a in $Archs){
+        ./dotnet/build-package-requirement.ps1 -RepoName $RepoName -ProjectDir $ProjectDir -Name $Name -Configuration "Release" -Arch $a
+    }
+}
+else{
+    ./dotnet/build-package-requirement.ps1 -RepoName $RepoName -ProjectDir $ProjectDir -Name $Name -Configuration "Release" -Arch $Arch
+}
 $RepoPath = [IO.Path]::Combine($pwd, $RepoName)
 
 if (Test-Path -Path "$RepoPath\$ProjectDir\windows") {
