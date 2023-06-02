a2beffbc534957bb973f071a560d289ad1cb66b6
Artur51D <106963910+Artur51D@users.noreply.github.com>
2023-06-02 08:57:14 +0100

    Update build-package-requirements.ps1

--- ci/build-package-requirements.ps1
+++ ci/build-package-requirements.ps1
@@ -7,17 +7,22 @@ param(
 )
 
 if($IsLinux){
+    # Because .NET does not officially support the x86 architecture on Linux distributions, we are using the x64 job to build x86 binaries.
     $Archs = @("x86", "x64")
     foreach($a in $Archs){
         ./dotnet/build-package-requirement.ps1 -RepoName $RepoName -ProjectDir $ProjectDir -Name $Name -Configuration "Release" -Arch $a
+        
+        # CMake generates build files specific to the architecture. We are deleting the build folder to ensure clean build enviroment. 
         Remove-Item -LiteralPath "$RepoPath/$ProjectDir/build" -Force -Recurse -ErrorAction SilentlyContinue
     }
 }
 else{
     ./dotnet/build-package-requirement.ps1 -RepoName $RepoName -ProjectDir $ProjectDir -Name $Name -Configuration "Release" -Arch $Arch
 }
+
 $RepoPath = [IO.Path]::Combine($pwd, $RepoName)
 
+# Verify that the path to binaries exists and copy it to package-files folder  
 if (Test-Path -Path "$RepoPath\$ProjectDir\windows") {
     $Subfolder = "windows"
 }
