05c9029a04b4d94e509696aff6e4104f6d02e8e1
Ben51Degrees <Ben@51Degrees.com>
2023-06-20 16:11:43 +0100

    TEST: Aligned CI performance testing to other languages. Instead of using the results from testing a web server, the results from the performance example are used. This gives a better indication of the actual performance without the overhead of running a server in CI. This relates to #52

---
+++ ci/run-performance-tests-console.ps1
@@ -0,0 +1,117 @@
+param(
+    [Parameter(Mandatory=$true)]
+    [string]$RepoName,
+    [string]$OrgName,
+    [string]$Name = "Release_x64",
+    [string]$Configuration = "Release",
+    [string]$Arch = "x64"
+)
+
+
+$RepoPath = [IO.Path]::Combine($pwd, $RepoName)
+$PerfResultsFile = [IO.Path]::Combine($RepoPath, "test-results", "performance-summary", "results_$Name.json")
+$EvidenceFiles = [IO.Path]::Combine($pwd, $RepoName,"FiftyOne.DeviceDetection", "device-detection-cxx", "device-detection-data")
+$ExamplesRepoName = "device-detection-dotnet-examples"
+$ExamplesRepoPath = [IO.Path]::Combine($pwd, $ExamplesRepoName)
+$DeviceDetectionProject = [IO.Path]::Combine($RepoPath, "FiftyOne.DeviceDetection", "FiftyOne.DeviceDetection", "FiftyOne.DeviceDetection.csproj")
+$PerfProject = [IO.Path]::Combine($ExamplesRepoPath, "Examples", "OnPremise", "Performance-Console")
+
+Write-Output "Entering '$RepoPath'"
+Push-Location $RepoPath
+
+try {
+
+    # Create the output directories if they don't already exist.
+    if ($(Test-Path -Path "test-results") -eq  $False) {
+        mkdir test-results
+    }
+    if ($(Test-Path -Path "test-results/performance-summary") -eq  $False) {
+        mkdir test-results/performance-summary
+    }
+
+}
+finally {
+
+    Write-Output "Leaving '$RepoPath'"
+    Pop-Location
+
+}
+
+try {
+    if ($(Test-Path -Path $ExamplesRepoName) -eq $False) {
+        Write-Output "Cloning '$ExamplesRepoName'"
+        ./steps/clone-repo.ps1 -RepoName $ExamplesRepoName -OrgName $OrgName
+    }
+
+    Write-Output "Moving TAC file"
+    $TacFile = [IO.Path]::Combine($EvidenceFiles, "TAC-HashV41.hash") 
+    Copy-Item $TacFile device-detection-dotnet-examples/device-detection-data/TAC-HashV41.hash
+
+    Write-Output "Moving evidence file"
+    $EvidenceFile = [IO.Path]::Combine($EvidenceFiles, "20000 Evidence Records.yml")
+    Copy-Item $EvidenceFile "device-detection-dotnet-examples/device-detection-data/20000 Evidence Records.yml"
+    
+    $ExamplesProject = [IO.Path]::Combine($ExamplesRepoPath, "Examples", "ExampleBase")
+    
+    # Restore nuget packages in the examples project
+    Write-Output "Entering '$ExamplesRepoPath'"
+    Push-Location $ExamplesRepoPath
+    try {
+
+        Write-Output "Running Nuget Restore"
+        nuget restore
+    }
+    finally {
+
+        Write-Output "Leaving '$ExamplesRepoPath'"
+        Pop-Location
+    }
+    
+    # Update the dependency in the examples project to point to the newly bulit package
+    Write-Output "Entering '$ExamplesProject'"
+    Push-Location $ExamplesProject
+    try{
+        # Change the dependency version to the locally build Nuget package
+        Write-Output "Replacing the DeviceDetection package with a local reference."
+        dotnet remove package "FiftyOne.DeviceDetection"
+        dotnet add reference $DeviceDetectionProject
+    }
+    finally{
+        Write-Output "Leaving '$ExamplesProject'"
+        Pop-Location
+    }
+    
+    # Build the project now that all is set up
+    Write-Output "Building project with following configuration '$Configuration|$Arch|$BuildMethod'"
+    ./device-detection-dotnet-examples/ci/build-project.ps1 -RepoName $ExamplesRepoName -Name $Name -Configuration $Configuration -Arch $Arch -BuildMethod $BuildMethod
+    
+    Write-Output "Running performance example"
+    Write-Output "Entering '$PerfProject' folder"
+    Push-Location "$PerfProject"
+    try {
+        dotnet run -c $Configuration -d $TacFile -u $EvidenceFile -j summary.json
+        
+        # Write out the results for comparison
+        Write-Output "Writing performance test results"
+        $Results = Get-Content ./summary.json | ConvertFrom-Json
+        Write-Output "{
+            'HigherIsBetter': {
+                'DetectionsPerSecond': $($Results.MaxPerformance.DetectionsPerSecond)
+            },
+            'LowerIsBetter': {
+                'MsPerDetection': $($Results.MaxPerformance.MsPerDetection)
+            }
+        }" > $PerfResultsFile
+
+    }
+    finally {
+        Write-Output "Leaving '$PerfProject'"
+        Pop-Location
+    }
+
+    Copy-Item $ExamplesRepoName/test-results $RepoName -Recurse
+}
+
+finally {
+    exit $LASTEXITCODE
+}
---
+++ ci/run-performance-tests-web.ps1
@@ -0,0 +1,110 @@
+param(
+    [Parameter(Mandatory=$true)]
+    [string]$RepoName,
+    [string]$ProjectDir = ".",
+    [string]$Name = "Release_x64",
+    [string]$Configuration = "Release",
+    [string]$Arch = "x64"
+)
+
+
+$RepoPath = [IO.Path]::Combine($pwd, $RepoName)
+$PerfPath = [IO.Path]::Combine($RepoPath, "performance-tests")
+$PerfResultsFile = [IO.Path]::Combine($RepoPath, "test-results", "performance-summary", "results_$Name.json")
+
+Write-Output "Entering '$RepoPath'"
+Push-Location $RepoPath
+
+try {
+
+    # Create the output directories if they don't already exist.
+    if ($(Test-Path -Path "test-results") -eq  $False) {
+        mkdir test-results
+    }
+    if ($(Test-Path -Path "test-results/performance-summary") -eq  $False) {
+        mkdir test-results/performance-summary
+    }
+
+}
+finally {
+
+    Write-Output "Leaving '$RepoPath'"
+    Pop-Location
+
+}
+
+Write-Output "Setting Data File for testing"
+
+$SettingsFile = [IO.Path]::Combine($RepoPath, "performance-tests", "appsettings.json") 
+
+# Read the contents of the appsettings.json file
+$json = Get-Content -Path $SettingsFile | ConvertFrom-Json
+
+# Update the "DataFile" value
+$json.PipelineOptions.Elements[0].BuildParameters.DataFile = $env:DEVICEDETECTIONDATAFILE
+
+# Convert the updated JSON object back to a string
+$jsonString = $json | ConvertTo-Json -Depth 10
+
+# Write the updated JSON string back to the appsettings.json file
+$jsonString | Set-Content -Path $SettingsFile
+
+dotnet publish "$PerfPath" -c $Configuration.Replace("Core","") /p:Platform=$Arch -o "$PerfPath/output" /p:BuiltOnCI=true
+
+if($IsLinux){
+    #install APR library for linux
+    sudo apt-get install apache2-dev libapr1-dev libaprutil1-dev
+}
+
+Write-Output "Entering '$PerfPath'"
+Push-Location $PerfPath
+
+try {
+    mkdir build
+    Push-Location build
+    try {
+
+        # Build the performance tests
+        Write-Output "Building performance test"
+        cmake ..
+        cmake --build .
+
+        # When running the performance tests, set the data file name manually,
+        # then unset once we're done
+        Write-Output "Running performance test"
+
+        if($IsLinux){
+            $dotnetPath = whereis dotnet
+            Write-Output $dotnetPath
+            bash ./runPerf.sh $Arch $Configuration.Replace("Core","") $dotnetPath.Split(" ")[1]
+        }
+        else{
+            ./runPerf.ps1 -c $Configuration.Replace("Core","") -p $Arch
+        }
+		
+        Get-ChildItem -Path $PerfPath -Filter "summary.json" -File -Recurse | ForEach-Object {
+            $destinationPath = Join-Path -Path $PerfPath/build -ChildPath $_.Name
+            Copy-Item -Path $_.FullName -Destination $destinationPath -Force -ErrorAction SilentlyContinue
+            Write-Host "Copied $($_.Name) to $destinationPath"
+        }
+	
+        # Output the results as it's useful for debugging.
+        $files = Get-ChildItem -Filter "*.out" -File -Recurse
+        foreach ($file in $files) {
+            Write-Output "$($file.Name) :"
+            Get-Content $file
+        }
+
+    }
+    finally {
+        Write-Output "Leaving build"
+        Pop-Location
+
+    }
+}
+finally {
+
+    Write-Output "Leaving '$PerfPath'"
+    Pop-Location
+
+}
