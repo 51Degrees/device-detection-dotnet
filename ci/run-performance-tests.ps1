e5397a27e65ea62fa919ff24e104c9907b077f62
Ben51Degrees <Ben@51Degrees.com>
2023-06-20 16:30:58 +0100

    BUG: Use the correct path to the performance test script, as it is relative to common-ci not device-detection-dotnet.

--- ci/run-performance-tests.ps1
+++ ci/run-performance-tests.ps1
@@ -8,5 +8,5 @@ param(
     [string]$Arch = "x64"
 )
 
-. ./run-performance-tests-console.ps1 -RepoName $RepoName -OrgName $OrgName -Name $Name -Configuration $Configuration -Arch $Arch
-. ./run-performance-tests-web.ps1 -RepoName $RepoName -ProjectDir $ProjectDir -Name $Name -Configuration $Configuration -Arch $Arch
+. ./$RepoName/ci/run-performance-tests-console.ps1 -RepoName $RepoName -OrgName $OrgName -Name $Name -Configuration $Configuration -Arch $Arch
+. ./$RepoName/ci/run-performance-tests-web.ps1 -RepoName $RepoName -ProjectDir $ProjectDir -Name $Name -Configuration $Configuration -Arch $Arch
