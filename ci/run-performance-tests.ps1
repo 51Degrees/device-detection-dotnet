035afe86cfe1107bf928f0d4e2bf0ed8ab33dd0b
Ben51Degrees <Ben@51Degrees.com>
2023-06-20 16:13:37 +0100

    BUG: Removed unused parameter from the performance testing script.

--- ci/run-performance-tests.ps1
+++ ci/run-performance-tests.ps1
@@ -1,11 +1,12 @@
 param(
     [Parameter(Mandatory=$true)]
     [string]$RepoName,
+    [string]$OrgName,
     [string]$ProjectDir = ".",
     [string]$Name = "Release_x64",
     [string]$Configuration = "Release",
     [string]$Arch = "x64"
 )
 
-. ./run-performance-tests-console.ps1 -RepoName $RepoName -ProjectDir $ProjectDir -Name $Name -Configuration $Configuration -Arch $Arch
+. ./run-performance-tests-console.ps1 -RepoName $RepoName -OrgName $OrgName -Name $Name -Configuration $Configuration -Arch $Arch
 . ./run-performance-tests-web.ps1 -RepoName $RepoName -ProjectDir $ProjectDir -Name $Name -Configuration $Configuration -Arch $Arch
