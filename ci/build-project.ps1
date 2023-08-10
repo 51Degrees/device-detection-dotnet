dd4f45b153e2c50b3adddc6e77fd0b95d8bbbadc
Automation51D <CI@51Degrees.com>
2023-08-10 16:32:25 +0100

    BUILD: Added a solution filter, which loads only the dotnet core projects, and excludes the Framework ones.

--- ci/build-project.ps1
+++ ci/build-project.ps1
@@ -10,11 +10,16 @@ param(
 
 if ($BuildMethod -eq "dotnet"){
 
-    ./dotnet/build-project-core.ps1 -RepoName $RepoName -ProjectDir $ProjectDir -Name $Name -Configuration $Configuration -Arch $Arch
+    if ($IsWindows) {
+        ./dotnet/build-project-core.ps1 -RepoName $RepoName -ProjectDir $ProjectDir -Name $Name -Configuration $Configuration -Arch $Arch
+    }
+    else {
+        # On non-Windows, use the solution filter to remove the Framework projects.
+        ./dotnet/build-project-core.ps1 -RepoName $RepoName -ProjectDir "./FiftyOne.DeviceDetection.Core.slnf" -Name $Name -Configuration $Configuration -Arch $Arch
+    }
 
 }
 else{
-
     ./dotnet/build-project-framework.ps1 -RepoName $RepoName -ProjectDir $ProjectDir -Name $Name -Configuration $Configuration -Arch $Arch
 }
 
