ae024a1e0a6785c7fb9f60ee6c3441c3119e1e32
ben51degrees <ben@51degrees.com>
2023-08-11 16:00:17 +0100

    Update build-package-requirements.ps1

--- ci/build-package-requirements.ps1
+++ ci/build-package-requirements.ps1
@@ -5,42 +5,37 @@ param(
     [string]$RepoName,
     [string]$Arch
 )
+$RepoPath = [IO.Path]::Combine($pwd, $RepoName)
+
+$NativeName = "FiftyOne.DeviceDetection.Hash.Engine.OnPremise.Native.dll"
+$NativeFile = [IO.Path]::Combine($RepoPath, $ProjectDir, "build", $NativeName)
 
 if($IsLinux){
+    $Subfolder = "linux"
     # Because .NET does not officially support the x86 architecture on Linux distributions, we are using the x64 job to build x86 binaries.
     $Archs = @("x86", "x64")
     foreach($a in $Archs){
         ./dotnet/build-package-requirement.ps1 -RepoName $RepoName -ProjectDir $ProjectDir -Name $Name -Configuration "Release" -Arch $a
-        
+        $PackageFolder = "package-files/$SubFolder/$a"
+        New-Item -Path $PackageFolder -ItemType Directory -Force
+        Copy-Item -Path "$NativeFilesFolder" -Destination "$PackageFolder/$NativeName" -Force
+      
         # CMake generates build files specific to the architecture. We are deleting the build folder to ensure clean build enviroment. 
         Remove-Item -LiteralPath "$RepoPath/$ProjectDir/build" -Force -Recurse -ErrorAction SilentlyContinue
     }
 }
-else{
-    ./dotnet/build-package-requirement.ps1 -RepoName $RepoName -ProjectDir $ProjectDir -Name $Name -Configuration "Release" -Arch $Arch
-}
-
-$RepoPath = [IO.Path]::Combine($pwd, $RepoName)
-
-# Verify that the path to binaries exists and copy it to package-files folder  
-if ($IsWindows) {
+elseif ($IsWindows) {
     $Subfolder = "windows"
-}
-elseif ($IsLinux) {
-    $Subfolder = "linux"
+    ./dotnet/build-package-requirement.ps1 -RepoName $RepoName -ProjectDir $ProjectDir -Name $Name -Configuration "Release" -Arch $Arch
+    $PackageFolder = "package-files/$SubFolder/$Arch"
+    New-Item -Path $PackageFolder -ItemType Directory -Force
+    Copy-Item -Path "$NativeFilesFolder" -Destination "$PackageFolder/$NativeName" -Force
 }
 else {
-    Write-Host "No appropriate subfolder found."
+    Write-Host "Unsupported OS."
     exit
 }
-$NativeName = "FiftyOne.DeviceDetection.Hash.Engine.OnPremise.Native.dll"
-$NativeFile = [IO.Path]::Combine($RepoPath, $ProjectDir, "build", $NativeName)
 
-$PackageFolder = "package-files/$SubFolder/$Arch"
-New-Item -Path $PackageFolder -ItemType Directory -Force
-
-
-Copy-Item -Path "$NativeFilesFolder" -Destination "$PackageFolder/$NativeName" -Force
 
 
 exit $LASTEXITCODE
