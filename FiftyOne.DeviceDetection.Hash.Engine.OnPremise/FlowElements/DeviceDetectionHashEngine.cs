d2b7b77dc20168260a4e8458a7fcc85ac44cc464
ben51degrees <ben@51degrees.com>
2024-06-19 14:22:52 +0100

    FEAT: Added metrics properties to the CloudFree mapper.

--- FiftyOne.DeviceDetection.Hash.Engine.OnPremise/FlowElements/DeviceDetectionHashEngine.cs
+++ FiftyOne.DeviceDetection.Hash.Engine.OnPremise/FlowElements/DeviceDetectionHashEngine.cs
@@ -362,7 +362,7 @@ namespace FiftyOne.DeviceDetection.Hash.Engine.OnPremise.FlowElements
             "by the parent objects")]
         private IEnumerable<IFiftyOneAspectPropertyMetaData> GetMetricProperties()
         {
-            var dataFileList = new List<string>() { "Lite", "Premium", "Enterprise", "TAC" };
+            var dataFileList = new List<string>() { "Lite", "Premium", "Enterprise", "TAC", "CloudFree" };
             yield return new FiftyOneAspectPropertyMetaDataHash(
                 this,
                 "MatchedNodes",
