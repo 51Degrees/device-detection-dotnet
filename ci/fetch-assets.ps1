43c9118914a69b5af0d60a891176a5aca81c2628
Alexander Popel <alexander.popel@postindustria.com>
2025-12-02 16:21:48 +0200

    ci: use stable relative path in fetch-assets

--- ci/fetch-assets.ps1
+++ ci/fetch-assets.ps1
@@ -1,13 +1,12 @@
 param (
-    [Parameter(Mandatory)][string]$RepoName,
     [string]$DeviceDetection,
     [string]$DeviceDetectionUrl
 )
 $ErrorActionPreference = "Stop"
 
-$deviceDetectionData = "$RepoName/FiftyOne.DeviceDetection.Hash.Engine.OnPremise/device-detection-cxx/device-detection-data"
+$deviceDetectionData = "$PSScriptRoot/../FiftyOne.DeviceDetection.Hash.Engine.OnPremise/device-detection-cxx/device-detection-data"
 
-./steps/fetch-assets.ps1 -DeviceDetection $DeviceDetection -DeviceDetectionUrl $DeviceDetectionUrl `
+./steps/fetch-assets.ps1 -DeviceDetection:$DeviceDetection -DeviceDetectionUrl:$DeviceDetectionUrl `
     -Assets "TAC-HashV41.hash", "51Degrees-LiteV4.1.hash", "20000 Evidence Records.yml", "20000 User Agents.csv"
 
 Copy-Item "assets/TAC-HashV41.hash" $deviceDetectionData
