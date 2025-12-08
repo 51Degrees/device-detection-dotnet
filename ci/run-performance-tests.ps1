0079a22a29e585bce2a1e9821ba151c3dbe62913
Alexander Popel <alexander.popel@postindustria.com>
2025-12-08 11:04:22 +0200

    perf tests: switch to a custom httpbench script

--- ci/run-performance-tests.ps1
+++ ci/run-performance-tests.ps1
@@ -1,12 +1,10 @@
 param(
-    [Parameter(Mandatory=$true)]
-    [string]$RepoName,
+    [Parameter(Mandatory)][string]$RepoName,
     [string]$OrgName,
-    [string]$ProjectDir = ".",
     [string]$Name = "Release_x64",
     [string]$Configuration = "Release",
     [string]$Arch = "x64"
 )
 
-. ./$RepoName/ci/run-performance-tests-console.ps1 -RepoName $RepoName -OrgName $OrgName -Name $Name -Configuration $Configuration -Arch $Arch
-. ./$RepoName/ci/run-performance-tests-web.ps1 -RepoName $RepoName -ProjectDir $ProjectDir -Name $Name -Configuration $Configuration -Arch $Arch
+& $RepoName/ci/run-performance-tests-console.ps1 -RepoName:$RepoName -OrgName:$OrgName -Name:$Name -Configuration:$Configuration -Arch:$Arch
+& $RepoName/ci/run-performance-tests-web.ps1 -Name:$Name -Configuration:$Configuration -Arch:$Arch
