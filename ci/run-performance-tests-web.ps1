da4ac8902be482cf86dfd24beb8a9333d2492ef7
Alexander Popel <74662418+pi-alexander-popel@users.noreply.github.com>
2025-12-08 18:36:37 +0200

    Merged Pull Request '#651 fix-web-perf-tests->main: perf tests: switch to a custom httpbench script'
    * perf tests: switch to a custom httpbench script
    
    * performance-tests: use default config loading
    
    This will allow environment variable overrides to work properly. And
    using "dynamic relative paths" is confusing anyway.

--- ci/run-performance-tests-web.ps1
+++ ci/run-performance-tests-web.ps1
@@ -1,106 +1,37 @@
 param(
-    [Parameter(Mandatory=$true)]
-    [string]$RepoName,
-    [string]$ProjectDir = ".",
     [string]$Name = "Release_x64",
     [string]$Configuration = "Release",
     [string]$Arch = "x64"
 )
+$ErrorActionPreference = "Stop"
+$PSNativeCommandUseErrorActionPreference = $true
 
+$perfTests = "$PSScriptRoot/../performance-tests"
 
-$RepoPath = [IO.Path]::Combine($pwd, $RepoName)
-$PerfPath = [IO.Path]::Combine($RepoPath, "performance-tests")
+$env:PipelineOptions__Elements__0__BuildParameters__DataFile = $env:DEVICEDETECTIONDATAFILE
 
-Write-Output "Entering '$RepoPath'"
-Push-Location $RepoPath
-
+dotnet build $perfTests -c $Configuration /p:Platform=$Arch
 try {
-
-    # Create the output directories if they don't already exist.
-    if ($(Test-Path -Path "test-results") -eq  $False) {
-        mkdir test-results
-    }
-    if ($(Test-Path -Path "test-results/performance-summary") -eq  $False) {
-        mkdir test-results/performance-summary
-    }
-
-}
-finally {
-
-    Write-Output "Leaving '$RepoPath'"
-    Pop-Location
-
+    $server = dotnet "$perfTests/bin/$Arch/$Configuration/net8.0/performance_tests.dll" &
+    $results = ./scripts/httpbench.ps1 -HostPort '127.0.0.1:5000' -Endpoint '/process' -CalibrateEndpoint '/calibrate' -UaFile 'assets/20000 User Agents.csv'
+} finally {
+    Write-Host "--- Performance tests output ---"
+    Receive-Job $server
+    Stop-Job $server
 }
 
-# workaround, see https://github.com/actions/runner-images/issues/8598
-$env:PATH = $env:PATH -replace "C:\\Strawberry\\c\\bin;"
-
-Write-Output "Setting Data File for testing"
-
-$SettingsFile = [IO.Path]::Combine($RepoPath, "performance-tests", "appsettings.json") 
-
-# Read the contents of the appsettings.json file
-$json = (Get-Content -Path $SettingsFile) -replace '^\s*//.*' | Out-String | ConvertFrom-Json
-
-# Update the "DataFile" value
-$json.PipelineOptions.Elements[0].BuildParameters.DataFile = $env:DEVICEDETECTIONDATAFILE
-
-# Convert the updated JSON object back to a string
-$jsonString = $json | ConvertTo-Json -Depth 10
-
-# Write the updated JSON string back to the appsettings.json file
-$jsonString | Set-Content -Path $SettingsFile
-
-if($IsLinux){
-    #install APR library for linux
-    sudo apt-get install apache2-dev libapr1-dev libaprutil1-dev
+if ($results.overhead_ms -gt 200) {
+    Write-Error "Unacceptable request overhead: $($results.overhead_ms)"
+} elseif ($results.overhead_ms -lt 0) {
+    Write-Error "Request overhead shouldn't be negative: $($results.overhead_ms)"
 }
 
-Write-Output "Entering '$PerfPath'"
-Push-Location $PerfPath
-
-try {
-    mkdir build
-    Push-Location build
-    try {
-
-        # Build the performance tests
-        Write-Output "Building performance test"
-        cmake ..
-        cmake --build .
-
-        Write-Output "Building service"
-        dotnet build "$PerfPath" -c $Configuration /p:Platform=$Arch
-
-        # When running the performance tests, set the data file name manually,
-        # then unset once we're done
-        Write-Output "Running performance test"
-
-        ./runPerf.ps1 -c $Configuration -p $Arch 
-		
-        Get-ChildItem -Path $PerfPath -Filter "summary.json" -File -Recurse | ForEach-Object {
-            $destinationPath = Join-Path -Path $PerfPath/build -ChildPath $_.Name
-            Copy-Item -Path $_.FullName -Destination $destinationPath -Force -ErrorAction SilentlyContinue
-            Write-Host "Copied $($_.Name) to $destinationPath"
-        }
+# These results aren't used further, just output them
+ConvertTo-Json @{
+    'HigherIsBetter' = @{
+        'DetectionsPerSecond' = 1/($results.overhead_ms / 1000)
     }
-    finally {
-
-        # Output the results as it's useful for debugging.
-        $files = Get-ChildItem -Filter "*.out" -File -Recurse
-        foreach ($file in $files) {
-            Write-Output "$($file.Name) :"
-            Get-Content $file
-        }
-
-        Write-Output "Leaving build"
-        Pop-Location
-
+    'LowerIsBetter' = @{
+        'MsPerDetection' = $results.overhead_ms
     }
-}
-finally {
-
-    Write-Output "Leaving '$PerfPath'"
-    Pop-Location
-
 }
